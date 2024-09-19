538bb4826f2dd934aa246b884ccf124a9ddd2ec1
picuu <carloscapoforcada@gmail.com>
2024-09-19 16:25:54 +0200

    test: change tag from mined to inconclusive, the remaining mines counter increase

--- tests/minesweeper.web.test.js
+++ tests/minesweeper.web.test.js
@@ -248,23 +248,23 @@ defineFeature(feature, (test) => {
 
   test('Change tag from mined to inconclusive, the remaining mines counter increase', ({ given, and, when, then, pending }) => {
     given('the player opens the game', () => {
-      pending()
+      steps.openTheGame()
     })
 
     given('the player loads the following mock data', (docString) => {
-      pending()
+      steps.setMockData(docString)
     })
 
-    and(/^the player tags as mined the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
-      pending()
+    and(/^the player tags as mined the cell \("(.*)","(.*)"\)$/, (rowPosition, colPosition) => {
+      steps.tagCellAsMined(rowPosition, colPosition)
     })
 
-    when(/^the player tags as inconclusive the cell \("(.*)","(.*)"\)$/, (arg0, arg1) => {
-      pending()
+    when(/^the player tags as inconclusive the cell \("(.*)","(.*)"\)$/, (rowPosition, colPosition) => {
+      steps.tagCellAsInconclusive(rowPosition, colPosition)
     })
 
-    then(/^the remaining mines counter should be "(.*)"$/, (arg0) => {
-      pending()
+    then(/^the remaining mines counter should be "(.*)"$/, (mineCounterValue) => {
+      expect(steps.getMineCounterValue()).toEqual(Number(mineCounterValue))
     })
   })
 
