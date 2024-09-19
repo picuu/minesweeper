6ce4ffe2f1f14c518bda7433fe8af671cb09297c
picuu <carloscapoforcada@gmail.com>
2024-09-19 11:22:20 +0200

    feat: refactor statusButton to dynamically display different faces based on game status

--- src/components/statusButton.jsx
+++ src/components/statusButton.jsx
@@ -1,9 +1,24 @@
 import '@/components/styles/statusButton.css'
+import { useSelector } from 'react-redux'
 
 export function StatusButton () {
+  const { gameStatus } = useSelector(state => state.gameStatus)
+
+  const getFace = (status) => {
+    if (status === 'waiting' || status === 'playing') {
+      return { fileName: 'happyFace', altText: 'happy face' }
+    } else if (status === 'won') {
+      return { fileName: 'winFace', altText: 'win face' }
+    } else if (status === 'lost') {
+      return { fileName: 'deadFace', altText: 'dead face' }
+    }
+  }
+
+  const { fileName, altText } = getFace(gameStatus)
+
   return (
     <button data-testid='status-button' className='status-button'>
-      <img src='/faces/happyFace.png' alt='happy face' />
+      <img src={`/faces/${fileName}.png`} alt={altText} />
     </button>
   )
 }
