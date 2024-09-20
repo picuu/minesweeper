b542a07636bacd182a4b2c2d4c4c869631e8ca0a
picuu <carloscapoforcada@gmail.com>
2024-09-20 11:57:13 +0200

    feat: mineCounter component can be negative

--- src/components/mineCounter.jsx
+++ src/components/mineCounter.jsx
@@ -3,11 +3,32 @@ import { useSelector } from 'react-redux'
 
 export function MineCounter () {
   const { remainingFlags } = useSelector(state => state.remainingFlags)
-  const numberOfMines = () => Number(remainingFlags)
 
-  const firstDigit = parseInt((numberOfMines() % 1000) / 100)
-  const secondDigit = parseInt((numberOfMines() % 100) / 10)
-  const thirdDigit = parseInt(numberOfMines() % 10)
+  const generateDigits = () => {
+    let numberOfMines = Number(remainingFlags)
+
+    let isNegative = false
+    if (numberOfMines < 0) {
+      numberOfMines *= -1
+      isNegative = true
+    }
+
+    let firstDigit = parseInt((numberOfMines % 1000) / 100)
+    let secondDigit = parseInt((numberOfMines % 100) / 10)
+    const thirdDigit = parseInt(numberOfMines % 10)
+
+    if (isNegative) {
+      if (numberOfMines >= 10) {
+        firstDigit = '-'
+      } else {
+        secondDigit = '-'
+      }
+    }
+
+    return { firstDigit, secondDigit, thirdDigit }
+  }
+
+  const { firstDigit, secondDigit, thirdDigit } = generateDigits()
 
   return (
     <div className='mineCounter' data-testid='mineCounter'>
